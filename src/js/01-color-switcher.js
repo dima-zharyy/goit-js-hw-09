efe6e70e89e64c0931c33142aa73a235ff53bb0b
dima-zharyy <zharyydimka@gmail.com>
2022-05-26 19:38:55 +0300

    added-color-switcher-task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+refs.stopBtn.setAttribute('disabled', 'true');
 
+const SWITCH_INTERVAL = 500;
+let bgSwitcherId = null;
+
+refs.startBtn.addEventListener('click', onStartSwitcher);
+refs.stopBtn.addEventListener('click', onStopSwitcher);
+
+function onStartSwitcher() {
+  bgSwitcherStart();
+  switchDisabled();
+}
+
+function onStopSwitcher() {
+  bgSwitcherStop();
+  switchDisabled();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function bgSwitcherStart() {
+  bgSwitcherId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, SWITCH_INTERVAL);
+}
+
+function bgSwitcherStop() {
+  clearInterval(bgSwitcherId);
+}
+
+function switchDisabled() {
+  if (refs.startBtn.hasAttribute('disabled')) {
+    refs.startBtn.removeAttribute('disabled');
+    refs.stopBtn.setAttribute('disabled', 'true');
+  } else {
+    refs.stopBtn.removeAttribute('disabled');
+    refs.startBtn.setAttribute('disabled', 'true');
+  }
+}
