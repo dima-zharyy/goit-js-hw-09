1f378c30134151dc2ef0efe7273cf38c1ef2ee44
dima-zharyy <zharyydimka@gmail.com>
2022-05-28 18:06:46 +0300

    refactored-promises-task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,6 +6,13 @@ const refs = {
 
 refs.form.addEventListener('submit', onSubmit);
 
+function onSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = getFormData(event.currentTarget);
+
+  createPromisesFromData(delay, step, amount);
+}
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -24,11 +31,19 @@ function createPromise(position, delay) {
   });
 }
 
-function onSubmit(event) {
-  event.preventDefault();
-  const { delay, step, amount } = getFormData(event.currentTarget);
+function getFormData(eventCurrentTarget) {
+  const inputData = new FormData(eventCurrentTarget);
+  const outputData = {};
 
-  let iterableDelay = Number(delay);
+  inputData.forEach((value, name) => {
+    outputData[name] = Number(value);
+  });
+
+  return outputData;
+}
+
+function createPromisesFromData(delay, step, amount) {
+  let iterableDelay = delay;
 
   for (let i = 1; i <= amount; i += 1) {
     if (i === 1) {
@@ -39,7 +54,7 @@ function onSubmit(event) {
         .catch(({ position, delay }) => {
           Notify.failure(`Rejected promise ${position} in ${delay}ms`);
         });
-      iterableDelay += Number(step);
+      iterableDelay += step;
       continue;
     }
 
@@ -51,17 +66,6 @@ function onSubmit(event) {
         Notify.failure(`Rejected promise ${position} in ${delay}ms`);
       });
 
-    iterableDelay += Number(step);
+    iterableDelay += step;
   }
-}
-
-function getFormData(eventCurrentTarget) {
-  const inputData = new FormData(eventCurrentTarget);
-  const outputData = {};
-
-  inputData.forEach((value, name) => {
-    outputData[name] = value;
-  });
-
-  return outputData;
 }
